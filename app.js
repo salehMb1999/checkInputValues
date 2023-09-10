f77322c059b83bb8d631df2d272cfecb16bc424b
salehmb1999 <salehmirbagheri78@gmail.com>
2023-09-10 14:23:02 +0430

    added some features and styles

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
 const $ = document;
 let userName = $.getElementById("userName");
 let passWord = $.getElementById("passWord");
-let btn = $.querySelector("span");
+let btn = $.querySelector(".btn");
 let modal = $.querySelector(".modal");
-let closeModal = $.querySelector("i");
-closeModal.addEventListener("click", () => {
-    modal.style.transform = "translateY(-50rem)";
-});
+let userNameAlert = $.querySelector(".userNameAlert");
+let passWordAlert = $.querySelector(".passWordAlert");
+
 btn.addEventListener("click", () => {
   if (userName.value.length < 12 || passWord.value.length < 8) {
-    modal.style.transform = "translateY(-12rem)";
+    modal.style.top = "10rem";
+    setTimeout(() => {
+      modal.style.top = "-10rem";
+      userName.value = "";
+      passWord.value = "";
+    }, 3000);
   } else {
-    alert("nice");
+    modal.innerHTML = "خوش آمدید";
+    modal.style.backgroundColor = "rgb(11, 67, 11)";
+    modal.style.top = "10rem";
+    setTimeout(() => {
+      modal.style.top = "-10rem";
+      userName.value = "";
+      passWord.value = "";
+    }, 3000);
+  }
+});
+userName.addEventListener("keyup", () => {
+  if (userName.value.length < 12) {
+    userName.style.border = "8px solid red";
+    userNameAlert.style.visibility = "visible"
+  } else {
+    userName.style.border = "8px solid #2ed573";
+    userNameAlert.style.visibility = "hidden"
+
+  }
+});
+passWord.addEventListener("keyup", () => {
+  if (passWord.value.length < 8) {
+    passWord.style.border = "8px solid red";
+    passWordAlert.style.visibility = "visible"
+
+  } else {
+    passWord.style.border = "8px solid #2ed573";
+    passWordAlert.style.visibility = "hidden"
+
   }
 });
